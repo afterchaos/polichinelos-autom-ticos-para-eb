3c9dfad0243b4eaecbcce6f28d0a4675f4c4c646
neto <alimarnetto@gmail.com>
2026-02-28 15:46:01 -0300

    Melhorias visuais nos arquivos novamente

--- compilar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Instalando dependencias...
-pip install requests packaging pyinstaller
-echo.
-echo Compilando o AutoJJS...
-pyinstaller --onefile --noconsole --name "AutoJJS" --icon=NONE main.py
-echo.
-echo Compilacao concluida! O arquivo esta na pasta 'dist'.
-pause
